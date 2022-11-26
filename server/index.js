2e4202f9b066df8317595cdeaec8c5358acf3899
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-11-25 22:09:02 -0500

    cors header

--- server/index.js
+++ server/index.js
@@ -7,6 +7,25 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://mernfront.vercel.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const mongoose = require("mongoose")
 mongoose.connect(`mongodb+srv://ulu:${process.env.REACT_APP_MONGODB}@cluster0.exuzikf.mongodb.net/mernproject?retryWrites=true&w=majority`)
 
