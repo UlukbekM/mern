87bb05469fbf0fe9977d2c6994ea854eabf1dbb4
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-11-27 18:37:52 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mernfront.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
