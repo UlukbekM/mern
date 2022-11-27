efb60a3662a616857d890f8f5a2f210de08290de
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-11-27 18:29:50 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -5,17 +5,17 @@ require('dotenv').config()
 
 const cors = require('cors')
 app.use(express.json())
-// app.use(cors(
-//     {
-//         origin: ['mernfront.vercel.app', 'localhost:3000'],
-//         methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-//     }
-// ))
+// app.use(cors())
+app.use(cors(
+    {
+        origin: ['mernfront.vercel.app', 'localhost:3000'],
+        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    }
+))
 // app.options('*', cors())
 
 //https://www.npmjs.com/package/cors#enabling-cors-pre-flight
 
-app.use(cors())
 
 const mongoose = require("mongoose")
 mongoose.connect(`mongodb+srv://ulu:${process.env.REACT_APP_MONGODB}@cluster0.exuzikf.mongodb.net/mernproject?retryWrites=true&w=majority`)
