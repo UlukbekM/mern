6b5d56644d2dcd6d60c2ac58334e8a74a185e264
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-11-27 18:40:37 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -7,11 +7,9 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
